f67b335735a5ef4dee5a799cf6aaf321fc48c1df
John Dopping <john.dh@hotmail.co.uk>
2026-07-17 23:36:31 +0100

    Refactor attempt 2

--- game.py
+++ game.py
@@ -1,3 +1,40 @@
+
+def show_status():
+    print("\n===== Soldiers =====")
+
+    for soldier in soldiers.values():
+        print(
+            f"{soldier.name:<8}"
+            f" HP:{soldier.health:<3}"
+            f" Ammo:{soldier.ammo:<2}"
+        )
+
+def choose_soldier(prompt, current_player):
+    while True:
+        name = input(prompt)
+
+        if name not in soldiers:
+            print("Invalid soldier.")
+            continue
+
+        chosen = soldiers[name]
+
+        if chosen is current_player:
+            print("You cannot choose yourself.")
+            continue
+
+        return chosen
+
+soldiers = {
+    "John": Soldier("John", 100, 30),
+    "Dave": Soldier("Dave", 80, 50),
+    "Kaya": Soldier("Kaya", 90, 40),
+    "Medic": Medic("Medic", 85, 20)
+}
+
+turn_order = list(soldiers.values())
+turn = 0
+
 enemy = choose_soldier(
     "Choose an enemy: ",
     current_player,
@@ -5,4 +42,4 @@ enemy = choose_soldier(
 )
 
 current_player.take_turn(enemy)
-
+run_game()
