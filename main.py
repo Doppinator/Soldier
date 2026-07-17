413c1aa8e7b86ff51ce03ae881101c27463a9184
John Dopping <john.dh@hotmail.co.uk>
2026-07-17 23:48:46 +0100

    refactor 4

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-from soldier import Soldier, Medic, show_status, choose_soldier 
+from game import run_game
 
-soldiers = {
-    "John": Soldier("John", 100, 30),
-    "Dave": Soldier("Dave", 80, 50),
-    "Kaya": Soldier("Kaya", 90, 40),
-    "Medic": Medic("Medic", 85, 20)
-}
-
-turn_order = list(soldiers.values())
-turn = 0
-
-
-
-while True:
-    current_player = turn_order[turn]
-    show_status()
-    print(f"\n===== {current_player.name}'s turn =====")
-
-    current_player.take_turn()
-
-    # Move to the next player
-    turn = (turn + 1) % len(turn_order)
+run_game()
